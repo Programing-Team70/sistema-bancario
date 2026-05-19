cbfedc24cf8e845a7a40250008e84e5088acbd64
dmonterroso-2021543 <dmonterroso-2021543@kinal.edu.gt>
2026-05-18 20:00:23 -0600

    feat: configurar sistema de navegación, ciclo de autenticación y punto de entrada de la app.

--- BancoKing/bank_client_web/src/app/router/AppRouter.jsx
+++ BancoKing/bank_client_web/src/app/router/AppRouter.jsx
@@ -5,7 +5,10 @@ import { RoleGuard } from './RoleGuard.jsx';
 import { LoginPage } from '../../features/auth/pages/LoginPage.jsx';
 import { Users } from '../../features/users/components/Users.jsx';
 import { VerifyEmailPage } from '../../features/auth/pages/VerifyEmailPage.jsx';
+import { Movements } from '../../features/accounts/components/Movements.jsx';
 import { Accounts } from '../../features/accounts/components/Accounts.jsx';
+import { Transfers } from '../../features/transfers/components/Transfers.jsx';
+import { Withdrawals } from '../../features/withdrawals/components/Withdrawals.jsx';
 
 export const AppRouter = () => {
   return (
@@ -16,18 +19,57 @@ export const AppRouter = () => {
         path='/dashboard/*'
         element={
           <ProtecterRoute>
-            <RoleGuard allowedRoles={['ADMIN_ROLE']}>
-              <DashboardPage />
-            </RoleGuard>
+            <DashboardPage />
           </ProtecterRoute>
         }
       >
         {/*
             Nota: Las rutas hijas faltantes, se colocaran aquí más adelante.
         */}
+        <Route
+          path='users'
+          element={
+            <RoleGuard allowedRoles={['ADMIN_ROLE']}>
+              <Users />
+            </RoleGuard>
+          }
+        />
 
-        <Route path='users' element={<Users />} />
-        <Route path='accounts' element={<Accounts />} />
+        <Route
+          path='movements'
+          element={
+            <RoleGuard allowedRoles={['ADMIN_ROLE']}>
+              <Movements />
+            </RoleGuard>
+          }
+        />
+
+        <Route
+          path='accounts'
+          element={
+            <RoleGuard allowedRoles={['ADMIN_ROLE']}>
+              <Accounts />
+            </RoleGuard>
+          }
+        />
+
+        <Route
+          path='transfers'
+          element={
+            <RoleGuard allowedRoles={['ADMIN_ROLE', 'USER_ROLE']}>
+              <Transfers />
+            </RoleGuard>
+          }
+        />
+
+        <Route
+          path='withdrawals'
+          element={
+            <RoleGuard allowedRoles={['ADMIN_ROLE']}>
+              <Withdrawals />
+            </RoleGuard>
+          }
+        />
       </Route>
     </Routes>
   );
